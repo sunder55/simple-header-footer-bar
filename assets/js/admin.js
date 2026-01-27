822fb9ccd208732fdcabd1d33db21e6876714ef4
Rukmagat Kandel <101416363+sunder55@users.noreply.github.com>
2026-01-27 17:44:27 +0545

    Added settings fields
    The settings fields has been added
    Added color picker for background and font color

---
+++ assets/js/admin.js
@@ -0,0 +1,3 @@
+jQuery(document).ready(function ($) {
+  $(".shfb-color-field").wpColorPicker();
+});
--- includes/class-shfb-settings.php
+++ includes/class-shfb-settings.php
@@ -22,11 +22,11 @@ class SHFB_Settings
      */
     public function __construct()
     {
-        $this->options = get_options($this->option_key, []);
+        $this->options = get_option($this->option_key, []);
 
         // admin hooks 
         add_action('admin_menu', [$this, 'add_settings_page']);
-
+        add_action('admin_init', [$this, 'register_settings']);
         // enqueue scripts
         add_action('admin_enqueue_scripts', [$this, 'enqueue_scripts']);
     }
@@ -40,7 +40,9 @@ class SHFB_Settings
         if ($hook != 'settings_page_shfb-settings') {
             return;
         }
-        wp_enqueue_script('shfb-admin-script', SHFB_URL . 'assets/js/admin.js', ['jquery'], time(), true);
+        wp_enqueue_style('wp-color-picker');
+        wp_enqueue_script('shfb-admin-js', SHFB_URL . 'assets/js/admin.js', ['wp-color-picker', 'jquery'], time(), true);
+
         wp_enqueue_style('shfb-admin-style', SHFB_URL . 'assets/css/admin.css', [], time());
     }
 
@@ -50,8 +52,8 @@ class SHFB_Settings
     public function add_settings_page()
     {
         add_options_page(
-            'Simple Header Footer Bar Settings',
-            'Header Footer Bar',
+            __('Header Footer Bar', 'simple-header-footer-bar'),
+            __('Header Footer Bar', 'simple-header-footer-bar'),
             'manage_options',
             'shfb-settings',
             [$this, 'render_settings_page']
@@ -61,8 +63,132 @@ class SHFB_Settings
     /**
      * Option page callback
      */
+    public function register_settings()
+    {
+        register_setting('shfb_options_group', $this->option_key, [$this, 'sanitize_settings']);
+        add_settings_section(
+            'shfb_main_section',
+            __('Bar Settings', 'simple-header-footer-bar'),
+            null,
+            'shfb-settings'
+        );
+
+        $fields = [
+            'text'         => __('Bar Text', 'simple-header-footer-bar'),
+            'bg_color'     => __('Background Color', 'simple-header-footer-bar'),
+            'font_color'   => __('Font Color', 'simple-header-footer-bar'),
+            'position'     => __('Position', 'simple-header-footer-bar'),
+            'close_button' => __('Show Close Button', 'simple-header-footer-bar'),
+        ];
+
+        foreach ($fields as $field_id => $field_title) {
+            $callback = match ($field_id) {
+                'text'         => [$this, 'text_field'],
+                'bg_color', 'font_color' => [$this, 'color_field'],
+                'position'     => [$this, 'position_field'],
+                'close_button' => [$this, 'checkbox_field'],
+            };
+
+            add_settings_field(
+                $field_id,
+                $field_title,
+                $callback,
+                'shfb-settings',
+                'shfb_main_section',
+                ['id' => $field_id]
+            );
+        }
+    }
+
+    /**
+     * Sanitize settings before saving
+     */
+    public function sanitize_settings($input)
+    {
+        $output = [];
+
+        $output['text']         = isset($input['text']) ? wp_kses_post($input['text']) : '';
+        $output['bg_color']     = isset($input['bg_color']) ? sanitize_hex_color($input['bg_color']) : '#000000';
+        $output['font_color']   = isset($input['font_color']) ? sanitize_hex_color($input['font_color']) : '#ffffff';
+        $output['position']     = in_array($input['position'] ?? '', ['header', 'footer'], true) ? $input['position'] : 'header';
+        $output['close_button'] = ! empty($input['close_button']) ? 1 : 0;
+
+        return $output;
+    }
+
+    /**
+     * Render settings page
+     */
     public function render_settings_page()
     {
-        echo 'plguin page created';
+?>
+        <div class="wrap">
+            <h1><?php esc_html_e('Header Footer Bar Settings', 'simple-header-footer-bar'); ?></h1>
+            <form method="post" action="options.php">
+                <?php
+                settings_fields('shfb_options_group');
+                do_settings_sections('shfb-settings');
+                submit_button();
+                ?>
+            </form>
+        </div>
+    <?php
+    }
+
+    /**
+     * Render text input
+     */
+    public function text_field($args)
+    {
+        $id = $args['id'];
+        printf(
+            '<input type="text" id="%1$s" name="%2$s[%1$s]" value="%3$s" class="regular-text">',
+            esc_attr($id),
+            esc_attr($this->option_key),
+            esc_attr($this->options[$id] ?? '')
+        );
+    }
+
+    /**
+     * Render color input
+     */
+    public function color_field($args)
+    {
+        $id = $args['id'];
+        printf(
+            '<input type="text" id="%1$s" name="%2$s[%1$s]" value="%3$s" class="shfb-color-field">',
+            esc_attr($id),
+            esc_attr($this->option_key),
+            esc_attr($this->options[$id] ?? '')
+        );
+    }
+
+    /**
+     * Render select for position
+     */
+    public function position_field($args)
+    {
+        $value = $this->options['position'] ?? 'header';
+    ?>
+        <select name="<?php echo esc_attr($this->option_key); ?>[position]">
+            <option value="header" <?php selected($value, 'header'); ?>>Header</option>
+            <option value="footer" <?php selected($value, 'footer'); ?>>Footer</option>
+        </select>
+<?php
+    }
+
+    /**
+     * Render checkbox for close button
+     */
+    public function checkbox_field($args)
+    {
+        $id = $args['id'];
+        $checked = ! empty($this->options[$id]) ? 'checked' : '';
+        printf(
+            '<input type="checkbox" id="%1$s" name="%2$s[%1$s]" value="1" %3$s>',
+            esc_attr($id),
+            esc_attr($this->option_key),
+            $checked
+        );
     }
 }
