b506dd042d7d8917c49b37931e89942b0ddca791
Rukmagat Kandel <101416363+sunder55@users.noreply.github.com>
2026-01-27 14:39:56 +0545

    Added requires files and folder

--- simple-header-footer-bar.php
+++ simple-header-footer-bar.php
@@ -1,23 +1,24 @@
  <?php 
  /**
   * Plugin Name: Simple Header Footer Bar
-  * Plugin URI: https://github.com/sunderkandel/simple-header-footer-bar
-  * Description: A simple plugin to add header and footer to your website.
+  * Plugin URI: https://github.com/sunder55/simple-header-footer-bar
+  * Description: A simple plugin to add header and footer bar to your website.
   * Version: 1.0.0
-  * Author: Sunder Kandel
-  * Author URI: https://sunderkandel.com
+  * Author: Rukmagat Kandel
   * License: GPL-2.0+
   * License URI: https://www.gnu.org/licenses/gpl-2.0.txt
   * Text Domain: simple-header-footer-bar
-  * Domain Path: /languages
   */
 
   if (!defined('ABSPATH')) {
-    define('SIMPLE_HEADER_FOOTER_BAR_VERSION', '1.0.0');
-    define('SIMPLE_HEADER_FOOTER_BAR_DIR', __DIR__);
-    define('SIMPLE_HEADER_FOOTER_BAR_PATH', __DIR__ .'/');
+   exit;
   }
 
-  require_once SIMPLE_HEADER_FOOTER_BAR_DIR . '/includes/class-simple-header-footer-bar.php';
+/*
+* plugin constants
+*/
+define( 'SHFB_VERSION', '1.0.0' );
+define( 'SHFB_PATH', plugin_dir_path( __FILE__ ) );
+define( 'SHFB_URL', plugin_dir_url( __FILE__ ) );
+define( 'SHFB_BASENAME', plugin_basename( __FILE__ ) );
 
-  
